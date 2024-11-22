3bd044d3c562067bcc1dc8a96d3e0b8704292ca2
CLAUDIOTOLEDO252208 <toledo.claudio.adrian@gmail.com>
2024-11-22 15:20:37 -0300

    modifique error en el deploy

--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const clienteController = require("../controllers/ClienteController");
+const clienteController = require("../controllers/clienteController");
 
 // Rutas CRUD para clientes
 router.post("/", clienteController.crearCliente);
