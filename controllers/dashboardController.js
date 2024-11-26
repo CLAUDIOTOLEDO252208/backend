c6c9788ade5566ed3577b4cdcb49e2e24bfbf90f
CLAUDIOTOLEDO252208 <toledo.claudio.adrian@gmail.com>
2024-11-26 12:24:35 -0300

    agrego cantidadAsesores a dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,6 +4,7 @@ const Credito = require("../models/Credito"); // Ajusta al nombre correcto del m
 const obtenerResumen = async (req, res) => {
   try {
     const cantidadClientes = await Cliente.countDocuments();
+    const cantidadAsesores = await Asesor.countDocuments();
     const cantidadCreditos = await Credito.countDocuments();
     const cantidadCreditosCancelados = await Credito.countDocuments({
       estado: "cancelado", // Asegúrate de que coincida con tu base de datos
@@ -15,6 +16,7 @@ const obtenerResumen = async (req, res) => {
     res.status(200).json({
       cantidadClientes,
       cantidadCreditos,
+      cantidadAsesores,
       cantidadCreditosCancelados,
       cantidadCreditosPendientes,
     });
